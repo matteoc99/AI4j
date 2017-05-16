573d087786b81d95cff9d521073388209112e71a
matteoc99 <matteo.cosi@live.it>
2017-05-16 14:07:03 +0200

    Network rework

--- GameAi/src/neuralNetworkLib/Network.java
+++ GameAi/src/neuralNetworkLib/Network.java
@@ -19,6 +19,19 @@ public class Network {
     private Network(ArrayList<Layer> layers){
         this();
         if (layers!=null)
-        this.layers=layers;
+         this.layers=layers;
     }
+    public Network(int inputSize,int outputSize,int hiddenAmmount,int... hiddenSize){
+        this();
+        if(hiddenSize.length!=hiddenAmmount)
+            throw new IllegalArgumentException("hiddenSize count not rigth");
+    }
+    public Network createDFF(int inputSize,int outputSize,int hiddenAmmount,int hiddenSize){
+        int [] hidden=new int[hiddenAmmount];
+        for (int i = 0; i <hiddenAmmount; i++) {
+            hidden[i]=hiddenSize;
+        }
+        return new Network(inputSize,outputSize,hiddenAmmount,hidden);
+    }
+
 }
