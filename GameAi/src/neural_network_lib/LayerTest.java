389c21347854c98672d5fde209f3d032a2726f53
matteoc99 <matteo.cosi@live.it>
2017-05-21 21:57:53 +0200

    setting tests up

--- GameAi/src/neural_network_lib/LayerTest.java
+++ GameAi/src/neural_network_lib/LayerTest.java
@@ -1,8 +1,62 @@
 package neural_network_lib;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author Matteo Cosi
  * @since 21.05.2017
  */
 public class LayerTest {
+
+    @Test
+    public void getType() {
+
+    }
+
+    @Test
+    public void setType() {
+
+    }
+
+    @Test
+    public void removeNeuron() {
+
+    }
+
+    @Test
+    public void addNeuron() {
+
+    }
+
+    @Test
+    public void addNeurons() {
+
+    }
+
+    @Test
+    public void connectWith() {
+
+    }
+
+    @Test
+    public void getNeuronCount() {
+
+    }
+
+
+    @Test
+    public void feed() {
+
+    }
+
+    @Test
+    public void send() {
+
+    }
+
+    @Test
+    public void getNeuronAt() {
+
+    }
+
 }
--- GameAi/src/neural_network_lib/NetworkTest.java
+++ GameAi/src/neural_network_lib/NetworkTest.java
@@ -1,8 +1,63 @@
 package neural_network_lib;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 /**
  * @author Matteo Cosi
  * @since 21.05.2017
  */
 public class NetworkTest {
+    @Test
+    public void addLayer() {
+
+    }
+
+    @Test
+    public void removeLayer() {
+
+    }
+
+
+    @Test
+    public void getLayerByIndex() {
+
+    }
+
+    @Test
+    public void connect() {
+
+    }
+
+    @Test
+    public void getIndexOfLayer() {
+
+    }
+
+    @Test
+    public void processData() {
+
+    }
+
+
+    @Test
+    public void getDescriptorLength() {
+
+    }
+
+    @Test
+    public void generateDescriptor() {
+
+    }
+
+    @Test
+    public void getTotalNeuronCount() {
+
+    }
+
+    @Test
+    public void getAllNeurons() {
+
+    }
 }
