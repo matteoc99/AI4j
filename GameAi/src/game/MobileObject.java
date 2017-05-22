8995b4ea639f9883ba1c135c18f0a1f67ecbe09f
matteoc99 <matteo.cosi@live.it>
2017-05-22 11:43:19 +0200

    test push

--- GameAi/src/game/MobileObject.java
+++ GameAi/src/game/MobileObject.java
@@ -38,7 +38,7 @@ public class MobileObject extends ImmobileObject {
 
     /**
      * Construktor for the {@link MobileObject}
-     *
+     *add comment
      * @param dateiname image to load in the {@link JComponent}
      */
     public MobileObject(String dateiname) throws ClassNotFoundException {
