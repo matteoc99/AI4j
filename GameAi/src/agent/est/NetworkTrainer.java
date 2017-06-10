2d12af39c8bcb93ba5e38863269de124e767612b
Maximilian <eschtlmax2@gmail.com>
2017-06-10 09:49:12 +0200

    Universal .iml and .idea files

--- GameAi/src/agent/est/NetworkTrainer.java
+++ GameAi/src/agent/est/NetworkTrainer.java
@@ -2,6 +2,7 @@ package agent.est;
 
 import game.PlayGround;
 import neural_network_lib.Network;
+import neural_network_lib.Utils;
 
 import java.util.ArrayList;
 
@@ -21,37 +22,50 @@ public class NetworkTrainer {
         new NetworkTrainer().init();
     }
 
-    void init() {
+    private void init() {
         // create startNetworks
         for (int i = 0; i < amountOfSurvivors; i++) {
-            agents.add(new EstAgent(Network.createDFF(5,2,2,5)));
+            agents.add(new EstAgent(Network.createDFF(5,2,1,5)));
         }
+
         for (;;) {
-            processNetworks();
-            pickNetworks();
-            cloneAgents();
+            processAgents();
+            pickAgents();
+            cloneAndMutateAgents();
         }
     }
 
-    void processNetworks() {
+    private void processAgents() {
         EstAgent[] agentsArray = new EstAgent[agents.size()];
         agents.toArray(agentsArray);
         new PlayGround(null, agentsArray);
-    }
-
-    void pickNetworks() {
-        agents.sort((o1, o2) -> o1.getFitness()>=o2.getFitness()? 1:-1);
-        for (int i = amountOfSurvivors; i < agents.size(); i++) {
-            agents.remove(i);
+        while (!PlayGround.trainingOver) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    void cloneAgents() {
+    private void pickAgents() {
+        agents.sort((o1, o2) -> o1.getFitness()<o2.getFitness()? 1:-1);
+        int size = agents.size();
+        for (int i = size-1; i >= amountOfSurvivors; i--) {
+            agents.remove(i);
+        }
+        System.out.println(agents.get(0).getFitness());
+    }
+
+    private void cloneAndMutateAgents() {
         ArrayList<EstAgent> newAgents = new ArrayList<>();
+        for (EstAgent agent : agents) {
+            newAgents.add(new EstAgent(new Network(agent.getNet().getDescriptor())));
+        }
         for (int survivorIndex = 0; survivorIndex < amountOfSurvivors; survivorIndex++) {
             double[] descriptor = agents.get(survivorIndex).getNet().getDescriptor();
             for (int childNumber = 0; childNumber<amountOfChildren/amountOfSurvivors; childNumber++) {
-                newAgents.add(new EstAgent(new Network(descriptor)));
+                newAgents.add(new EstAgent(new Network(Utils.mutate(descriptor))));
             }
         }
         agents = newAgents;
