986410aa386f114eb8156190b8f1088dc8a3742d
Maximilian <eschtlmax2@gmail.com>
2017-08-31 17:35:50 +0200

    Refactored EvoShooter-Field-Components out of Field

--- EvoShooter/src/values/Values.java
+++ EvoShooter/src/values/Values.java
@@ -9,10 +9,6 @@ public abstract class Values {
     public static final int FLAG_RADIUS = 4;
     public static final int SPAWN_RADIUS = 4;
 
-    // K_MAX * K_MIN must be 1
-    public static final double K_MAX = 10000;  // 10 ^  |x|
-    public static final double K_MIN = 0.0001; // 10 ^ -|x|
-
     public static final int POSSIBLE_SPAWN_POSITIONS_COUNT = 24;
 
     public static final int REACH_FLAG_MINIMUM_PERCENTAGE = 20;
